ef4f243e3f3b6b4eaccf377dba388ca5da216ced
robherron <rlherron100@gmail.com>
2020-09-18 23:02:57 -0400

    Clean up

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,16 +1,33 @@
-const gallery = document.getElementById('gallery')
-const modalArray = [];
-const cards = [];
+const gallery = document.getElementById('gallery'); //creates variable for gallery div
+const modalArray = []; //creates an empty array in which future modal objects will be stored
+const cards = []; //creates an empty array in which future card objects will be stored
+
+
+
+///FETCH REQUESTS
+fetchProfile('https://randomuser.me/api/?results=12') //fetches 12 random users from the randomuser api
+
 
 ///HELPER FUNCTIONS
 
+
+//fetchProfile function handles fetch requests
+function fetchProfile (url) {
+    fetch(url)
+        .then(response => response.json()) //parses response to json
+        .then(data => {displayCard(data); getModals(data);addEvents(); closeModal()}) //sends the parsed data to the various functions
+        
+}
+
+
 //displayCard displays the 12 profiles sent back from the fetch request
 function displayCard (data) {
-    for (i = 0; i<data.results.length; i++) {
-        const card = document.createElement('DIV');
+    for (i = 0; i<data.results.length; i++) { //runs through each of the .results objects in the returned data
+        const card = document.createElement('DIV'); //creates a new card for each user profile
         card.classList = "card";
         gallery.appendChild(card);
-        const html = `<div class="card-img-container">
+        //html is the template literal for what the cards will look like
+        const html = `<div class="card-img-container">          
                     <img class="card-img" src='${data.results[i].picture.medium}' alt="profile picture">
                 </div>
                 <div class="card-info-container">
@@ -19,8 +36,8 @@ function displayCard (data) {
                     <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
                 </div>
             </div>`
-            card.innerHTML=html; 
-            cards.push(card);
+            card.innerHTML=html; //displays the cards
+            cards.push(card); //adds the cards to the empty cards array
         }
 }
 
@@ -28,7 +45,8 @@ function displayCard (data) {
 function getModals(data) {
     
    
-    for (i=0; i<data.results.length; i++) {
+    for (i=0; i<data.results.length; i++) {//runs through each of the .results objects in the returned data
+        //modal is a template literal for the modal windows
         const modal =  `<div class="modal-container">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
@@ -44,51 +62,26 @@ function getModals(data) {
             </div>
         </div>
     </div>`
-
-    
-        modalArray.push(modal);
-
-        
-
+        modalArray.push(modal); //adds the modal windows, with their html template, into the modalArray array for further use
     }
-   
 }
-
-//fetchProfile function handles fetch requests
-function fetchProfile (url) {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {displayCard(data); getModals(data);addEvents(); closeModal()})
-        .then()
-        
-}
-
-
-function saveModals (modals) {
-    const modalsArray = modals.map(x=>x)
-    return modalsArray
-}
-
-///FETCH REQUESTS
-fetchProfile('https://randomuser.me/api/?results=12')
-
-
 
 ///EVENT HANDLERS
-function addEvents () {
-    for (i=0; i<cards.length; i++) {
+function addEvents () {             //an event handler function that listens at each card element for a click event, upon which it displays a modal card
+    for (i=0; i<cards.length; i++) { //runs through each card and adds a click listener to each
         cards[i].addEventListener('click', (e) => {
-            const clickedCardIndex = [...cards].indexOf(e.currentTarget);
+            const clickedCardIndex = [...cards].indexOf(e.currentTarget); //identifies which card has been clicked
             const clickedModal = modalArray[clickedCardIndex];
-            gallery.insertAdjacentHTML('afterbegin', clickedModal)
+            gallery.insertAdjacentHTML('afterbegin', clickedModal) //displays an adjacent html template over the gallery with the 
+                                                            //modal window that corresponds to the clicked card
             
         });
         
 }};
 
-function closeModal () {
+function closeModal () {            //event handler function listens for a click event on the X button of the modal windows, and closes the modal window
    
-    gallery.addEventListener('click', (e) => {
+    gallery.addEventListener('click', (e) => { //targets both the button and the 'x' text and closes the modal window if they are clicked
     const modalClose = document.getElementById('modal-close-btn');
     const modalContainer = document.getElementsByClassName('modal-container')[0];
         if ((e.target === modalClose) || (e.target === modalClose.children[0])) {
@@ -100,172 +93,3 @@ function closeModal () {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function cardMod (data) {
-//     const card = document.getElementsByClassName('card');
-
-//     for (i = 0; i<card.length; i++) {
-//         card[i].addEventListener('click', () => {
-//             for (i = 0; i<card.length; i++) {
-//                 const modalContainer = document.createElement('DIV');
-//                 modalContainer.classList = "modal-container";
-//                 document.querySelector('body').appendChild(modalContainer);
-//                 //TROUBLE CONNECTING CARD VARIABLE TO THE REST OF FUNCTION AS EVENT LISTENER IS NESTED SO VALUES NESTED ELSEWHERE AREN'T VALID
-//                 const modHTML = `<div class="modal">
-//                                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//                                     <div class="modal-info-container">
-//                                         <img class="modal-img" src='${data.results[i].picture.large}' alt="profile picture">
-//                                         <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
-//                                         <p class="modal-text">${data.results[i].email}</p>
-//                                         <p class="modal-text cap">${data.results[i].location.city}</p>
-//                                         <hr>
-//                                         <p class="modal-text">${data.results[i].cell}</p>
-//                                         <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-//                                         <p class="modal-text">Birthday: 10/21/2015</p>
-//                                     </div>`
-//                                     modalContainer.innerHTML = modHTML;}
-                
-                
-//                 }) }
-
-// };
-
-
-
-
-
-// Promise.all([
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-//     fetchProfile('https://randomuser.me/api'),
-// ])
-// .catch(error => console.log('Looks like there was a problem!', error))
-
-
-
-// const card = document.createElement('DIV');
-// card.classList = "card";
-// document.getElementById('gallery').appendChild(card);
-// const html = `<div class="card-img-container">
-//             <img class="card-img" src='${data.results[0].picture.medium}' alt="profile picture">
-//         </div>
-//         <div class="card-info-container">
-//             <h3 id="name" class="card-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-//             <p class="card-text">${data.results[0].email}</p>
-//             <p class="card-text cap">${data.results[0].location.city}, ${data.results[0].location.state}</p>
-//         </div>
-//     </div>`
-//     card.innerHTML=html;
-    
-
-
-
-// card.addEventListener('click', () => {
-        //     const modalContainer = document.createElement('DIV');
-        //     modalContainer.classList = "modal-container";
-        //     document.querySelector('body').appendChild(modalContainer);
-            
-        //     const modHTML = `<div class="modal">
-        //                         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        //                         <div class="modal-info-container">
-        //                             <img class="modal-img" src='${data.results[0].picture.large}' alt="profile picture">
-        //                             <h3 id="name" class="modal-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-        //                             <p class="modal-text">${data.results[0].email}</p>
-        //                             <p class="modal-text cap">${data.results[0].location.city}</p>
-        //                             <hr>
-        //                             <p class="modal-text">${data.results[0].cell}</p>
-        //                             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-        //                             <p class="modal-text">Birthday: 10/21/2015</p>
-        //                         </div>`
-        //                         modalContainer.innerHTML = modHTML;
-            
-            
-        //     })
-
-
-
-
-
-// <!-- =======================
-//             Modal markup:
-
-//             You can use the commented out markup below as a template
-//             for your modal, but you must use JS to create and append 
-//             it to `body`.
-
-//             IMPORTANT: Altering the arrangement of the markup and the 
-//             attributes used may break the styles or functionality.
-
-//             <div class="modal-container">
-//                 <div class="modal">
-//                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//                     <div class="modal-info-container">
-//                         <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-//                         <h3 id="name" class="modal-name cap">name</h3>
-//                         <p class="modal-text">email</p>
-//                         <p class="modal-text cap">city</p>
-//                         <hr>
-//                         <p class="modal-text">(555) 555-5555</p>
-//                         <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-//                         <p class="modal-text">Birthday: 10/21/2015</p>
-//                     </div>
-//                 </div>
-
-//                 // IMPORTANT: Below is only for exceeds tasks 
-//                 <div class="modal-btn-container">
-//                     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-//                     <button type="button" id="modal-next" class="modal-next btn">Next</button>
-//                 </div>
-//             </div>
-//         ======================== --></div>
--- public_api_request-v1/Public-API-Requests/README.md
+++
@@ -1,2 +0,0 @@
-# Public API Requests
- A webpage that utilizes a random profile api to display 12 random profiles.
